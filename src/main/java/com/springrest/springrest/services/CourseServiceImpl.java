ced9769af54f61077e852bdfde552fc407cc6634
vrushali <vrushali.r@spenmo.com>
2023-07-13 14:19:16 +0530

    CRUD operation using srpingboot 2.7.13 framework added jpa layer

--- src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.springrest.springrest.services;
 
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,54 +12,66 @@ import java.util.stream.Collectors;
 @Service
 public class CourseServiceImpl implements CourseService {
 
-    List<Course> list;
+    @Autowired
+    private CourseDao courseDao;
+    //List<Course> list;
 
     public CourseServiceImpl() {
-        this.list = new ArrayList<>();
+        /*this.list = new ArrayList<>();
         list.add(new Course(1, "Java course", "This course contain basics of springboot"));
         list.add(new Course(2, "Golang course", "This course contain basics of golang gin framework"));
+
+         */
     }
 
     @Override
     public List<Course> getCourses() {
-        return list;
+        return courseDao.findAll();
     }
 
     @Override
     public Course getCourse(long courseId) {
-        Course c = null;
+        /*Course c = null;
         for (Course course : list) {
             if (course.getId() == courseId) {
                 c = course;
                 break;
             }
         }
-        return c;
+        return c;*/
+
+        return courseDao.getReferenceById(courseId);
     }
 
     @Override
     public Course addCourse(Course course) {
-        list.add(course);
+        // list.add(course);
+        courseDao.save(course);
         return course;
     }
 
     @Override
     public Course updateCourse(Course course) {
         //tried to use lamda function here on list to map and update list's course
-        list.forEach(e -> {
+       /* list.forEach(e -> {
             if (e.getId() == course.getId()) {
                 e.setTitle(course.getTitle());
                 e.setDescription(course.getDescription());
             }
         });
+        return course;*/
+
+        courseDao.save(course);
         return course;
     }
 
     @Override
     public void deleteCourse(Long courseId) {
-        list = this.list.stream()
+      /*  list = this.list.stream()
                 .filter(
                 e -> e.getId() != courseId)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList());*/
+        Course id = courseDao.getReferenceById(courseId);
+        courseDao.delete(id);
     }
 }
